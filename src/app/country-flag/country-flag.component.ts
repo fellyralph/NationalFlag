8ab01c3309063f0275daf0453fb91574824409bc
fellyralph <ralphfelly@gmail.com>
2016-11-17 17:48:02 +0100

    'country-flag'

--- src/app/country-flag/country-flag.component.ts
+++ src/app/country-flag/country-flag.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-country-flag',
+  selector: 'nf-country-flag',
   templateUrl: './country-flag.component.html'
 })
 export class CountryFlagComponent implements OnInit {
+          flagName : string ='Tanzanian National Flag';
+          flagUrl : string = 'http://tanzania-physicalresources.weebly.com/uploads/1/2/3/1/12310602/7136585_orig.jpg';
+
+
 
   constructor() { }
 

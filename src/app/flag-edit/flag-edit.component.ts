7d2053d18fc5fa8d84bba359385f7e65b91e9976
fellyralph <ralphfelly@gmail.com>
2016-11-29 11:21:58 +0100

    'addition-of-application-router-module'

---
+++ src/app/flag-edit/flag-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flag-edit',
+  templateUrl: './flag-edit.component.html',
+  styleUrls: ['./flag-edit.component.css']
+})
+export class FlagEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
